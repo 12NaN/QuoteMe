d3104f9090cb413d5f8c3380f46341292c8a319e
[12NaN] <frank.orefice25@students.kbcc.cuny.edu>
2019-08-18 18:40:17 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+
+    super(props);
+    this.state = {
+      quote: '',
+      author: ''
+    }
+    this.getNewQuote = this.getNewQuote.bind(this);
+  }
+  componentDidMount(){
+    this.getNewQuote();
+  }
+
+  getNewQuote(){
+      let url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+      axios.get(url)
+         .then(res => {
+            let data = res.data.quotes
+            let quoteNum = Math.floor(Math.random() * data.length) //quote number
+            let randomQuote = data[quoteNum] //actual quote
+
+            this.setState({
+               quote: randomQuote['quote'],
+               author: randomQuote['author']
+            })
+         })
+  }
+
+
+  render() {
+    const { quote, author } = this.state
+    return (
+      <div>
+          <h1>Quote-Me!</h1>
+          <h4>Click the button below to get a random quote!</h4>
+          <div id='text'><p>{quote}</p></div>
+          <div id='author'><h5>{author}</h5></div>
+          <div class = "wrapper">
+          <button class="button" onClick={this.getNewQuote}>New Quote</button>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
